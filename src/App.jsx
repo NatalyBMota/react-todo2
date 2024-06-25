b407392e6fd61613dfa39edf256286e828a16393
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-06-25 15:25:17 -0500

    Removed the todoList array of objects from the App.jsx file, as well as the unordered list that mapped that array of objects to a new array of objects, and that displayed properties from the objects in this new array, within list items. This functionality is now only performed through the TodoList component function, which is found within the TodoList.jsx file and imported into App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
 import './App.css';
 import TodoList from './TodoList.jsx';
 
-const todoList = [
-  {
-    id: 1,
-    title: 'Study React Components',
-  },
-  {
-    id: 2,
-    title: 'Review Array Map Method',
-  },
-  {
-    id: 3,
-    title: 'Complete One More Video-Based Course from Tree House',
-  }
-];
-
 const App = () => {
 
   return (
     <>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map(function(item) {
-          return <li key={item.id}>{item.title}</li>
-        })}
-      </ul>
       <TodoList />
     </>
   );
