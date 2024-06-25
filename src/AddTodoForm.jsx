77f7f93736882081361df18d11bc4de76db2f12b
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-06-25 15:45:47 -0500

    Wrote JSX for the return statement in the AddTodoForm function. Created a form element within in, as well as a label with the text 'Title.' Then, created a new text input element with the id of 'todoTitle.' Then, added the htmlFor attribute to the label element to associate it with the input field with the id of 'todoTitle.' Then, created a submit button element with the text 'Add.'

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,6 +1,10 @@
 const AddTodoForm = () => {
     return (
-        
+        <form>
+            <label htmlFor="todoTitle">Title</label>
+            <input type="text" id='todoTitle' />
+            <button type="submit">Add</button>
+        </form>
     );
 };
 
