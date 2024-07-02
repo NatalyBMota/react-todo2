ffb964e03356247d975a49c7a3e36eb36a60c3e8
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-07-02 16:59:01 -0500

    Opened /src/App.jsx. Inside the App functional component, above the return statement, created a new state variable named newTodo with update function named setNewTodo, while using the useState hook. To make that work, imported useState from react at the top of the file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { useState } from 'react';
 import TodoList from './TodoList.jsx';
 import AddTodoForm from './AddTodoForm.jsx';
 
 const App = () => {
-
+  [newTodo, setNewTodo] = useState("");
+  
   return (
     <>
       <h1>Todo List</h1>
