1a4a5e50a4756cf4f1ab6c4a75b9f06e1e940b2e
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-07-29 21:32:02 -0500

    Just realized that I was trying to get the TodoListItem component function to receive a prop called id as a parameter. Yet, I was not passing a prop with that name to it. So, I went into the TodoList.jsx file, where I was trying to use the TodoListItem component without first passing it that prop; and changed my code to fix that by adding id={item.id} to this component's JSX tag.

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -7,6 +7,7 @@ const TodoList = ({todoList, onRemoveTodo}) => {
               return (
                 <TodoListItem 
                   key={item.id} 
+                  id={item.id}
                   todo={item.title} 
                   onRemoveTodo={onRemoveTodo} 
                 />
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -2,7 +2,11 @@ const TodoListItem = ({id, todo, onRemoveTodo}) => {
     return (
         <li key={id}>
             {todo}
-            <button type="button" onClick={() => onRemoveTodo(id)}>Remove</button>
+            <button type="button" onClick={() => {
+                console.log("The remove button was clicked.");
+                console.log("id of the list item next to the clicked remove button", id);
+                onRemoveTodo(id)
+            }}>Remove</button>
         </li>
     );
 }
