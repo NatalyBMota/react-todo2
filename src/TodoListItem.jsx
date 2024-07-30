b1bf9f38d13e036ae5ad0f9d0041869890777bce
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-07-29 21:39:53 -0500

    Removed unecessary comments from the TodoListItem.jsx file, then added a couple of spaces between the todo item and the remove button that display once the user adds an item to the list.

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -2,9 +2,8 @@ const TodoListItem = ({id, todo, onRemoveTodo}) => {
     return (
         <li key={id}>
             {todo}
+            &nbsp;&nbsp;
             <button type="button" onClick={() => {
-                console.log("The remove button was clicked.");
-                console.log("id of the list item next to the clicked remove button", id);
                 onRemoveTodo(id)
             }}>Remove</button>
         </li>
