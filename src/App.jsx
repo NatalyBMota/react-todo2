c69f2257344a7614fd5806453a40f3d358215b44
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-08-05 20:11:24 -0500

    Did a bit of pair programming with mentor Travis O'Donnell during a one-on-one mentoring session on 8/5/2024. Might have completed all of the checklist items in the assignment's instructions that occurred before the header 'Add Loading State' during my Zoom video-conferencing meeting with him. Then, continued following the assignment's instruction; and might have completed all of the requirements outlined in lesson 1.7's instructions; although I still need to double check them.

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,39 @@ import { useState, useEffect} from 'react';
 import TodoList from './TodoList.jsx';
 import AddTodoForm from './AddTodoForm.jsx';
 
-const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem('savedTodoList')) ?? []);
-
-  useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList))
-  }, [todoList]);
-
-  return [todoList, setTodoList];
-};
+// Might have completed all of the requirements for the assignment
+// for lesson 1.7, but need to double-check to see if everything is
+// working as it should.
 
 const App = () => {
-  const [todoList, setTodoList] = useSemiPersistentState();
+
+  // const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem('savedTodoList')) ?? []);
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    let myNewPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        //resolve({data: {todoList: todoList}});
+        resolve({data: {todoList: JSON.parse(localStorage.getItem('savedTodoList'))}})
+      }, 2000);
+    })
+    .then(result => {
+      //setTodoList([...todoList, result.data.todoList]);
+      // result = {data: {todoList: JSON.parse(localStorage.getItem('savedTodoList'))}}
+      // result.data.todoList
+      setTodoList(result.data.todoList);
+      //console.log(result.data.todoList);
+      setIsLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+    }
+    // localStorage.setItem('savedTodoList', JSON.stringify(todoList))
+  }, [todoList]);
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
@@ -27,7 +48,7 @@ const App = () => {
 
   return (
     <>
-      <h1>Todo List</h1>
+      {isLoading ? (<p>Loading...</p>) : (<h1>Todo List</h1>)}
       <AddTodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
     </>
