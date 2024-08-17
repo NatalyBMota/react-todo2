a6dc859787d3f8be3aa5b10bf88579e0e7646469
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-08-16 21:12:20 -0500

    Declared another variable called todos, which accepts the results of mapping data.records into an array of todo objects that have the same schema as the existing todos. Used a snippet from the first code example that was provided in the assignment's direction to do this. My new code is now found between lines 41 and 49 of my App.jsx file. It is as follows: let todos = data.records.map(function(item) {         console.log(item.fields.title);         const newTodo =  {           id: item.id,           title: item.fields.title,         }         console.log(newTodo);         return newTodo       });

--- src/App.jsx
+++ src/App.jsx
@@ -37,8 +37,16 @@ const App = () => {
         // throw new Error(`${response.status}`);
       }
       let data = await response.json();
-      console.log(`Data variable: ${data}`);
       console.log(data);
+      let todos = data.records.map(function(item) {
+        console.log(item.fields.title);
+        const newTodo =  {
+          id: item.id,
+          title: item.fields.title,
+        }
+        console.log(newTodo);
+        return newTodo
+      });
     } catch {
 
     }
