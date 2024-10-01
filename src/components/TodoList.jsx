c57f2b1e33ee04c463a2f0c6e20a6c990d1ed328
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-09-30 20:17:26 -0500

    Moved the removeTodo logic from my TodoList.jsx file to my TodoListItem.jsx file, because it appeared to make more sense to have that code go there. I say so, because my TodoListItem.jsx file is the file that contains the remove button.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,32 +3,6 @@ import TodoListItem from "./TodoListItem.jsx";
 import styles from './TodoList.module.css';
 
 const TodoList = ({todoList, setTodoList}) => {
-  const removeTodo = async (id) => {
-    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}/${id}`;
-    
-    const options = {
-      method: 'DELETE',
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
-      },
-    };
-
-    try {
-      const response = await fetch(url, options);
-
-      if (!response.ok) {
-        let errorResponse = `${response.status}`;
-        throw new Error(errorResponse);
-      }
-
-      const filteredTodoList = todoList.filter((item) => item.id !== id);
-      setTodoList(filteredTodoList);
-    } catch (error) {
-      return null;
-    }
-  };
-
   return (
       <ul className={styles.list}>
           {todoList.map(function(item) {
@@ -37,7 +11,8 @@ const TodoList = ({todoList, setTodoList}) => {
                   key={item.id} 
                   id={item.id}
                   todo={item.title} 
-                  onRemoveTodo={removeTodo} 
+                  todoList={todoList}
+                  setTodoList={setTodoList}
                 />
               );
           })}
--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -2,13 +2,39 @@ import PropTypes from 'prop-types';
 import styles from './TodoListItem.module.css';
 import trashCan from '../assets/trash-can.svg';
 
-const TodoListItem = ({id, todo, onRemoveTodo}) => {
+const TodoListItem = ({ id, todo, todoList, setTodoList }) => {
+    const removeTodo = async (id) => {
+        const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}/${id}`;
+        
+        const options = {
+          method: 'DELETE',
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+          },
+        };
+    
+        try {
+          const response = await fetch(url, options);
+    
+          if (!response.ok) {
+            let errorResponse = `${response.status}`;
+            throw new Error(errorResponse);
+          }
+    
+          const filteredTodoList = todoList.filter((item) => item.id !== id);
+          setTodoList(filteredTodoList);
+        } catch (error) {
+          return null;
+        }
+      };
+
     return (
         <li key={id} className={styles.listItem}>
             {todo}
             &nbsp;&nbsp;
             <button type="button" onClick={() => {
-                onRemoveTodo(id)
+                removeTodo(id)
             }} className={styles.removeButton}><img src={trashCan} alt="Remove item from to-do list." className={styles.trashCanIcon} /></button>
         </li>
     );
