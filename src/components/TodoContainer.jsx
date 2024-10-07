ce9abadcdfad513f7be2c35d1d7532fc900cd5e2
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-10-07 18:34:06 -0500

    Added fetchData to the dependency Array of the useEffect function in order to get rid of a warning that said that 'React Hook useEffect has' that 'missing dependency.' Then, I got a new error message that said that I needed to wrap my fetchData in a useCallback. So, per Code the Dream React student Nihal Erdal's suggestion in a group mentoring session by CTD mentor Josh Goldberg, as well as through CTD mentor Gonzalo Vidal's suggestion, I imported useCallback; and wrapped that function in a useCallback method. Per Gonzalo Vidal's suggestion, I also passed a dependency array with isAscOrder inside it, to the useCallback React Hook, as a second argument to the method.

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Link } from "react-router-dom";
 import TodoList from './TodoList.jsx';
 import AddTodoForm from './AddTodoForm.jsx';
@@ -8,18 +8,15 @@ import checkListImg from '../assets/checklist.svg';
 const TodoContainer = () => {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [isAscOrder, setToggle] = useState(true);
+  const [isAscOrder, setisAscOrder] = useState(true);
 
   const sortAscCallBack = (objectA, objectB) => (objectA.title < objectB.title) ? -1 : (objectA.title > objectB.title) ? 1 : 0;
 
   const sortDescCallBack = (objectA, objectB) => (objectA.title < objectB.title) ? 1 : (objectA.title > objectB.title) ? -1 : 0;
 
   const toggleTitleSortOrder = () => {
-    console.log("ascOrderToggle before", isAscOrder);
     let reverseValueOfSortOrder = !isAscOrder; 
-    setToggle(reverseValueOfSortOrder);
-    console.log("ascOrderToggle after", reverseValueOfSortOrder);
-    console.log("Sorted Todo List", titleSortOrder(todoList, reverseValueOfSortOrder));
+    setisAscOrder(reverseValueOfSortOrder);
     setTodoList(titleSortOrder(todoList, reverseValueOfSortOrder));
   }
 
@@ -33,13 +30,14 @@ const TodoContainer = () => {
     }
   };
 
-  const fetchData = async () => {
+  const fetchData = useCallback(async () => {
+    const tableName = import.meta.env.VITE_TABLE_NAME;
     const tableViewToGetQueryParam = "view=Grid%20view";
     const sortQueryParam = "sort%5B0%5D%5Bfield%5D=title";
     const sortAscending = "asc";
     const sortDirectionQueryParam = `sort%5B0%5D%5Bdirection%5D=${sortAscending}`;
 
-    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}?${tableViewToGetQueryParam}&${sortQueryParam}&${sortDirectionQueryParam}`;
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${tableName}?${tableViewToGetQueryParam}&${sortQueryParam}&${sortDirectionQueryParam}`;
     
     
     const options = {
@@ -66,16 +64,16 @@ const TodoContainer = () => {
         return newTodo;
       });
       
-      setTodoList(titleSortOrder(todos, true));
+      setTodoList(titleSortOrder(todos, isAscOrder));
       setIsLoading(false);
     } catch (error) {
       return null;
     }
-  }
+  }, [isAscOrder]);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]);
 
   useEffect(() => {
     if (!isLoading) {
