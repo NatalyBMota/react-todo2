8972d969c790023defe2c604573f8e3e8f421cd3
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-10-08 16:57:02 -0500

    I just made a new step towards meeting final project requirements as outline in the rubric. The rubric mentions that TodoContainer.js [or .jsx] file should contain a 'function named removeTodo with' the ' parameter id' which uses 'Fetch API,' with the DELETE method to 'delete' a 'record from AirTable given' an 'id' for that record. I had that function in a grandchild component of the TodoContainer component function. That is, I had it inside of the TodoListItem component function, in the TodoListItem.jsx function. That component function's parent is the TodoList component function, which is a child of the TodoContainer component function. So, in my way to moving that function to the TodoContainer component function, I temporarily moved it to the TodoList component function. Then, I changed the props that were passed in to the TodoListItem accordingly, as well as the destructured list of props that TodoListItem received. I also changed the propTypes in towards the bottom of TodoListItem.jsx accordingly. Then, I tested my app in the browser to make sure that it still works; and it does.

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -2,32 +2,7 @@ import PropTypes from 'prop-types';
 import styles from './TodoListItem.module.css';
 import trashCan from '../assets/trash-can.svg';
 
-const TodoListItem = ({ id, todo, todoList, setTodoList }) => {
-    const removeTodo = async (id) => {
-        const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}/${id}`;
-        
-        const options = {
-          method: 'DELETE',
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
-          },
-        };
-    
-        try {
-          const response = await fetch(url, options);
-    
-          if (!response.ok) {
-            let errorResponse = `${response.status}`;
-            throw new Error(errorResponse);
-          }
-    
-          const filteredTodoList = todoList.filter((item) => item.id !== id);
-          setTodoList(filteredTodoList);
-        } catch (error) {
-          return null;
-        }
-      };
+const TodoListItem = ({ id, todo, removeTodo }) => {
 
     return (
         <li key={id} className={styles.listItem}>
@@ -43,8 +18,7 @@ const TodoListItem = ({ id, todo, todoList, setTodoList }) => {
 TodoListItem.propTypes = {
     id: PropTypes.string.isRequired,
     todo: PropTypes.string.isRequired,
-    todoList: PropTypes.array.isRequired,
-    setTodoList: PropTypes.func.isRequired,
+    removeTodo: PropTypes.func.isRequired
 };
 
 export default TodoListItem;
