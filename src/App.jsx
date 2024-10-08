064357be9029dd0d07a8522efa450b5e45e70b97
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-10-07 21:48:14 -0500

    Since the final project requirements state that the App.js [or App.jsx] file should contain a 'Switch component,' I created a new Switch component function in a file called Switch.jsx in the components folder and exported it as default. Then, I imported that component into the App.jsx file like this: 'import Switch from './components/Switch.jsx'.' Then, since the final project's rubric also stated that this Switch component should contain 'two or more Routes that are navigable,' including 'One or more routes which render a TodoList component;' and 'One route for 'home' or 'landing' page, I copied and pasted the code for those Routes from my App.jsx file to the return statement of my Switch component function, which is found in my Switch.jsx file. That made my application better meet these requirements.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import TodoContainer from './components/TodoContainer.jsx';
-import LandingPage from './components/LandingPage.jsx';
+import Switch from './components/Switch.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
-const App = () => { 
-  
-  const tableName = import.meta.env.VITE_TABLE_NAME;
-  
+const App = () => {    
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={
-          <TodoContainer tableName={tableName} />
-        }>
-        </Route>
-        <Route path="/landing" exact element={
-          <LandingPage />
-        }></Route>
-      </Routes>
+      <Switch />
     </BrowserRouter>
   );
 };
