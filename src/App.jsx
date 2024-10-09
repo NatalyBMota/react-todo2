03545a7d95ac5973daf3285e886e55432fd2a5a6
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-10-09 14:31:38 -0500

    To finish meeting the bonus final project requirement of having a 'navigation menu' in the App.js or App.jsx component, moved the navigation (or nav) element, as well as the footer element (which contains a link) from Switch.jsx to App.jsx. Then, since the Switch component's CSS module file only contained CSS code for JSX elements that had been moved to App.jsx, I renamed the Switch.module.css file App.module.css file, moved it to the styles folder, and imported it into the App.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css';
-import Switch from './components/Switch.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Switch from './components/Switch.jsx';
+import styles from './styles/App.module.css';
+import './styles/App.css';
 
 const App = () => {    
   return (
     <BrowserRouter>
+      <nav>
+          <Link to="/" alt="Render a todo list that can be edited." className={styles.navLink}>Todo List</Link>
+          <Link to="/landing" alt="Go to the landing page." className={styles.navLink}>Landing Page</Link>
+      </nav>
       <Switch />
+      <footer>
+          <Link to="https://icons8.com/icons/set/favicon" target="_blank" title="Where I got my fav (or favorite) icon from." className={styles.footerLink}>Fav Icons</Link>
+      </footer>
     </BrowserRouter>
   );
 };
--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,17 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import { Link } from "react-router-dom";
 import TodoContainer from './TodoContainer.jsx';
 import LandingPage from './LandingPage.jsx';
-import styles from './Switch.module.css';
 
 const Switch = () => {
     const tableName = import.meta.env.VITE_TABLE_NAME; 
     return (
         <>
-            <nav>
-                <Link to="/" alt="Render a todo list that can be edited." className={styles.navLink}>Todo List</Link>
-                <Link to="/landing" alt="Go to the landing page." className={styles.navLink}>Landing Page</Link>
-            </nav>
             <Routes>
                 <Route path="/" exact element={
                     <TodoContainer tableName={tableName} />
@@ -22,9 +16,6 @@ const Switch = () => {
                 }>
                 </Route>
             </Routes>
-            <footer>
-                <Link to="https://icons8.com/icons/set/favicon" target="_blank" title="Where I got my fav (or favorite) icon from." className={styles.footerLink}>Fav Icons</Link>
-            </footer>
         </>
     );
 };
