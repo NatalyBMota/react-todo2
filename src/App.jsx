f8754b98c8fc62bdb4c8a6335c30cb4d6ca5afd9
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2024-10-10 19:19:35 -0500

    Moved code from Switch.jsx back to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import TodoContainer from './components/TodoContainer.jsx';
+import LandingPage from './components/LandingPage.jsx';
 import { Link } from "react-router-dom";
-import Switch from './components/Switch.jsx';
 import styles from './styles/App.module.css';
 import './styles/App.css';
 
 const App = () => {    
+  const tableName = import.meta.env.VITE_TABLE_NAME; 
   return (
     <BrowserRouter>
       <nav>
           <Link to="/" alt="Render a todo list that can be edited." className={styles.navLink}>Todo List</Link>
           <Link to="/landing" alt="Go to the landing page." className={styles.navLink}>Landing Page</Link>
       </nav>
-      <Switch />
+      <Routes>
+            <Route path="/" exact element={
+                <TodoContainer tableName={tableName} />
+            }>
+            </Route>
+            <Route path="/landing" exact element={
+                <LandingPage />
+            }>
+            </Route> 
+      </Routes>
       <footer>
           <Link to="https://icons8.com/icons/set/favicon" target="_blank" title="Where I got my fav (or favorite) icon from." className={styles.footerLink}>Fav Icons</Link>
       </footer>
