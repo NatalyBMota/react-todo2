5d06a412402fcbf1574ac152d163a98e91715f46
Nataly Mota <interactivedesign@yahoo.com>
2024-10-15 20:28:18 -0500

    Fixed an error with the styling of the landing page. Also fixed an issue with a missing dependency for babel, which was causing a warning in the console.

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,7 +1,9 @@
+import styles from './LandingPage.module.css';
+
 const LandingPage = () => {
     return (
-        <main>
-            <h1>Landing Page</h1>
+        <main className={styles.mainForLandingPg}>
+            <h1 className={styles.h1ForLandingPg}>Landing Page</h1>
         </main>
     );
 };
